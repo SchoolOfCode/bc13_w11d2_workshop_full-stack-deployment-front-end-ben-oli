597d290f704c94ffd1fed2f39701d4681cbcf4d6
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-12-06 16:30:05 +0000

    eslint fixed for workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  eslintConfig: {
-    extends: ["react-app"],
-  },
+  extends: ["react-app"],
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {
+        "additional-typescript-only-rule": "warn",
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: 2015,
     sourceType: "module",
