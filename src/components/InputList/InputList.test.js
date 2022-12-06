e53fb6b393164d2b072daa57a9356129155dc512
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-12-06 15:59:41 +0000

    InputList test written for button

---
+++ src/components/InputList/InputList.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import InputList from "./index";
+
+describe("Check out input component has a button", () => {
+  test('Our button has the text of "Add to list"', () => {
+    // arrange
+
+    const buttonText = "Add to list";
+    render(<InputList addToList={() => jest.fn()} buttonText={buttonText} />);
+
+    // act
+    let button = screen.getByRole("button");
+
+    // assert
+    expect(button).toHaveTextContent(buttonText);
+  });
+});
